8d533dbda55a1291a5f59d184940f347f2ab9b59
Rodrigo Azevedo <134436718+Rodrig87@users.noreply.github.com>
2023-12-14 23:31:18 -0300

    Update Bike.java

--- Transportes/Bike.java
+++ Transportes/Bike.java
@@ -5,5 +5,12 @@ public class Bike extends NaoMotorizado {
 	public Bike(String _cor, String _modelo, double _preco) {
 		super(_cor, _modelo, _preco);
 	}
-
+int marcha = 1;
+	
+		void aumentar(int up) {
+			marcha += 1;
+		}
+		void reduzir(int down) {
+			marcha -= 1;
+		}
 }
